7e9b503c4f7a3f0a6c54a1f021bff5c9dcc4dbf6
paulokla <paulokla654@gmail.com>
2023-05-17 17:30:08 +0100

    app build

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  $("#nav-icon").click(function () {
+    $("ul").toggleClass("show");
+    $("body").toggleClass("hide-overflow");
+  });
+  const toTop = document.querySelector(".to-top");
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      toTop.classList.add("press");
+    } else {
+      toTop.classList.remove("press");
+    }
+  });
+  const currentpage = location.pathname;
+  console.log(currentpage);
+  const pages = document.querySelectorAll("nav ul a").forEach((link) => {
+    if (link.href.includes(`${currentpage}`)) {
+      link.classList.add("active");
+    }
+  });
+
+  $(".carousel").owlCarousel({
+    margin: 100,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: false,
+      },
+    },
+  });
+
+  window.addEventListener("mouseenter", () => {
+    if (window.offset > 100) {
+      gsap.fromTo(
+        ".all-contact",
+        { y: 300, opacity: 0 },
+        { opacity: 1, y: 0, duration: 3 }
+      );
+    }
+  });
+});
+
+function myFunction() {
+  document.getElementById("btn-read").onclick = location.href =
+    "OurServices.html";
+}
+
+// function contactBg(){
+//     document.getElementById("contact-section").style.backgroundColor= "var(--secondary-color)"
+//     document.getElementById("all-contacts").style.backgroundColor= "var(--primary-color)"
+// }
+
+// function contactBgLeave(){
+//     document.getElementById("contact-section").style.backgroundColor= "var(--primary-color)"
+//     document.getElementById("all-contacts").style.backgroundColor= "var(--secondary-color)"
+// }
